14d546865817b94c95e7d60da520438b6d7b4183
juan galarce <j6alarce@gmail.com>
2020-07-16 12:50:51 -0400

    pokedex con react + axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { getAllPokemon, getPokemon } from "./services/pokemon";
+import "./App.css";
+
+// Component
+import Card from "./components/Card/Card";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState("");
+  const [prevUrl, setPrevUrl] = useState("");
+  const [loading, setLoading] = useState(true);
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await getAllPokemon(initialURL);
+      setNextUrl(response.next);
+      setPrevUrl(response.previous);
+      await loadingPokemon(response.results);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  const next = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextUrl);
+    await loadingPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  };
+
+  const prev = async () => {
+    if (!prevUrl) return;
+    setLoading(true);
+    let data = await getAllPokemon(prevUrl);
+    await loadingPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  };
+
+  const loadingPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map(async (pokemon) => {
+        let pokemonRecord = await getPokemon(pokemon);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  console.log(pokemonData);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? (
+        <>
+          <Navbar />
+          <h1>Loading..</h1>
+        </>
+      ) : (
+        <>
+          <Navbar />
+          <div className="btn">
+            <button onClick={prev}>Anterior</button>
+            <button onClick={next}>Siguiente</button>
+          </div>
+          <div className="grid-container">
+            {pokemonData.map((pokemon, index) => {
+              return <Card key={index} pokemon={pokemon} />;
+            })}
+          </div>
+          <div className="btn">
+            <button onClick={prev}>Anterior</button>
+            <button onClick={next}>Siguiente</button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
